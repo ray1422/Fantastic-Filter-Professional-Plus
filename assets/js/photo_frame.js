82a8394739245189a285938dbd6416c9e1eac6a3
ray1422 <ray20020809@gmail.com>
2019-06-07 12:35:46 +0800

    update for gallery

---
+++ assets/js/photo_frame.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $(".photo_frame").addClass("origin")
+    $(".switch, .after").css('transition', '2s');
+    setTimeout(_ => {
+        $(".switch").css('opacity', 1);
+        $(".photo_frame").removeClass("origin");
+    }, 1000)
+    setTimeout(_ => {
+        $(".switch, .after").css('transition', '.3s');
+    }, 3000)
+    function updateSize() {
+        $(".photo_frame .pad").remove()
+        $(".photo_frame").each(function () {
+            $frame = $(this)
+            $pad = $('<div class="pad">').css('height', $frame.children("img").innerHeight())
+            $frame.append($pad)
+        })
+    }
+    updateSize();
+
+    $(".switch").click(function () {
+        $frame = $(this).parents(".photo_frame")
+        if ($frame.hasClass("origin")) {
+            $frame.removeClass("origin")
+        }
+        else {
+            $frame.addClass("origin")
+        }
+    })
+
+    $(window).resize(function () {
+        updateSize()
+    })
+})
